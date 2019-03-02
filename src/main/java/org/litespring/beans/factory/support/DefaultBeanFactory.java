eb09c00e77e803862a3a2dac0c1599c7815da358
Yu, Martin <dingxin.yu@uqconnect.edu.au>
2019-03-02 11:44:02 +0800

    liteSpring: add bean factory

---
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -0,0 +1,18 @@
+package org.litespring.beans.factory.support;
+
+import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanFactory;
+
+public class DefaultBeanFactory implements BeanFactory {
+
+    public DefaultBeanFactory(String s) {
+    }
+
+    public BeanDefinition getBeanDefinition(String petStore) {
+        return null;
+    }
+
+    public Object getBean(String beanId) {
+        return null;
+    }
+}
