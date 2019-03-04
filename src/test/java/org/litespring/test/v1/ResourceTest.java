5e61c4be78ac828d19d314b8f65cf3509186d463
Yu, Martin <dingxin.yu@uqconnect.edu.au>
2019-03-04 08:45:34 +0800

    liteSpring: add resources.io

---
+++ src/test/java/org/litespring/test/v1/ResourceTest.java
@@ -0,0 +1,51 @@
+package org.litespring.test.v1;
+
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.litespring.core.io.ClassPathResource;
+import org.litespring.core.io.FileSystemResource;
+import org.litespring.core.io.Resource;
+
+public class ResourceTest {
+
+    @Test
+    public void testClassPathResource() throws Exception {
+
+        Resource r = new ClassPathResource("petstore-v1.xml");
+
+        InputStream is = null;
+
+        try {
+            is = r.getInputStream();
+            //
+            Assert.assertNotNull(is);
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+
+    }
+
+    @Test
+    public void testFileSystemResource() throws Exception {
+
+		/*Resource r = new FileSystemResource("");
+
+		InputStream is = null;
+
+		try {
+			is = r.getInputStream();
+			//
+			Assert.assertNotNull(is);
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+*/
+    }
+
+}
