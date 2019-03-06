7f4d287a80b3faa85de238bec4f960255f335ef5
Yu, Martin <dingxin.yu@uqconnect.edu.au>
2019-03-06 08:53:40 +0800

    liteSpring: add property value

--- src/main/java/org/litespring/beans/factory/support/GenericBeanDefinition.java
+++ src/main/java/org/litespring/beans/factory/support/GenericBeanDefinition.java
@@ -1,6 +1,10 @@
 package org.litespring.beans.factory.support;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.PropertyValue;
 
 public class GenericBeanDefinition implements BeanDefinition {
     private String id;
@@ -8,6 +12,8 @@ public class GenericBeanDefinition implements BeanDefinition {
     private boolean singleton = true;
     private boolean prototype = false;
     private String scope = SCOPE_DEFAULT;
+
+    List<PropertyValue> propertyValues = new ArrayList<PropertyValue>();
 
     public GenericBeanDefinition(String id, String beanClassName) {
         this.id = id;
@@ -35,6 +41,10 @@ public class GenericBeanDefinition implements BeanDefinition {
         this.scope = scope;
         this.singleton = SCOPE_SINGLETON.equals(scope) || SCOPE_DEFAULT.equals(scope);
         this.prototype = SCOPE_PROTOTYPE.equals(scope);
+
     }
 
+    public List<PropertyValue> getPropertyValues() {
+        return this.propertyValues;
+    }
 }
