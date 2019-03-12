4917479c5a490b167787271f1c691f32fbb5698e
Yu, Martin <dingxin.yu@uqconnect.edu.au>
2019-03-12 08:45:28 +0800

    liteSpring: add constructor resolver

--- src/main/java/org/litespring/beans/factory/xml/XmlBeanDefinitionReader.java
+++ src/main/java/org/litespring/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -10,6 +10,7 @@ import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.ConstructorArgument;
 import org.litespring.beans.PropertyValue;
 import org.litespring.beans.factory.BeanDefinitionStoreException;
 import org.litespring.beans.factory.config.RuntimeBeanReference;
@@ -34,6 +35,10 @@ public class XmlBeanDefinitionReader {
     public static final String VALUE_ATTRIBUTE = "value";
 
     public static final String NAME_ATTRIBUTE = "name";
+
+    public static final String CONSTRUCTOR_ARG_ELEMENT = "constructor-arg";
+
+    public static final String TYPE_ATTRIBUTE = "type";
 
     BeanDefinitionRegistry registry;
 
@@ -60,6 +65,7 @@ public class XmlBeanDefinitionReader {
                 if (ele.attribute(SCOPE_ATTRIBUTE) != null) {
                     bd.setScope(ele.attributeValue(SCOPE_ATTRIBUTE));
                 }
+                parseConstructorArgElements(ele, bd);
                 parsePropertyElement(ele, bd);
                 this.registry.registerBeanDefinition(id, bd);
             }
@@ -77,6 +83,31 @@ public class XmlBeanDefinitionReader {
 
     }
 
+    public void parseConstructorArgElements(Element beanEle, BeanDefinition bd) {
+        Iterator iter = beanEle.elementIterator(CONSTRUCTOR_ARG_ELEMENT);
+        while (iter.hasNext()) {
+            Element ele = (Element) iter.next();
+            parseConstructorArgElement(ele, bd);
+        }
+
+    }
+
+    public void parseConstructorArgElement(Element ele, BeanDefinition bd) {
+
+        String typeAttr = ele.attributeValue(TYPE_ATTRIBUTE);
+        String nameAttr = ele.attributeValue(NAME_ATTRIBUTE);
+        Object value = parsePropertyValue(ele, bd, null);
+        ConstructorArgument.ValueHolder valueHolder = new ConstructorArgument.ValueHolder(value);
+        if (StringUtils.hasLength(typeAttr)) {
+            valueHolder.setType(typeAttr);
+        }
+        if (StringUtils.hasLength(nameAttr)) {
+            valueHolder.setName(nameAttr);
+        }
+
+        bd.getConstructorArgument().addArgumentValue(valueHolder);
+    }
+
     public void parsePropertyElement(Element beanElem, BeanDefinition bd) {
         Iterator iter = beanElem.elementIterator(PROPERTY_ELEMENT);
         while (iter.hasNext()) {
@@ -86,8 +117,6 @@ public class XmlBeanDefinitionReader {
                 logger.fatal("Tag 'property' must have a 'name' attribute");
                 return;
             }
-
-
             Object val = parsePropertyValue(propElem, bd, propertyName);
             PropertyValue pv = new PropertyValue(propertyName, val);
 
@@ -100,8 +129,6 @@ public class XmlBeanDefinitionReader {
         String elementName = (propertyName != null) ?
                 "<property> element for property '" + propertyName + "'" :
                 "<constructor-arg> element";
-
-
         boolean hasRefAttribute = (ele.attribute(REF_ATTRIBUTE) != null);
         boolean hasValueAttribute = (ele.attribute(VALUE_ATTRIBUTE) != null);
 
@@ -114,8 +141,10 @@ public class XmlBeanDefinitionReader {
             return ref;
         } else if (hasValueAttribute) {
             TypedStringValue valueHolder = new TypedStringValue(ele.attributeValue(VALUE_ATTRIBUTE));
+
             return valueHolder;
         } else {
+
             throw new RuntimeException(elementName + " must specify a ref or value");
         }
     }
