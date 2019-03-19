c3fe1a3aba24809476aa510f728632bccd989aef
Yu, Martin <dingxin.yu@uqconnect.edu.au>
2019-03-19 08:19:26 +0800

    liteSpring: scanner

--- src/main/java/org/litespring/beans/factory/support/GenericBeanDefinition.java
+++ src/main/java/org/litespring/beans/factory/support/GenericBeanDefinition.java
@@ -22,43 +22,44 @@ public class GenericBeanDefinition implements BeanDefinition {
         this.id = id;
         this.beanClassName = beanClassName;
     }
+    public GenericBeanDefinition() {
 
+    }
     public String getBeanClassName() {
 
         return this.beanClassName;
+    }
+    public void setBeanClassName(String className){
+        this.beanClassName = className;
     }
 
     public boolean isSingleton() {
         return this.singleton;
     }
-
     public boolean isPrototype() {
         return this.prototype;
     }
-
     public String getScope() {
         return this.scope;
     }
-
     public void setScope(String scope) {
         this.scope = scope;
         this.singleton = SCOPE_SINGLETON.equals(scope) || SCOPE_DEFAULT.equals(scope);
         this.prototype = SCOPE_PROTOTYPE.equals(scope);
 
     }
-
-    public List<PropertyValue> getPropertyValues() {
+    public List<PropertyValue> getPropertyValues(){
         return this.propertyValues;
     }
-
     public ConstructorArgument getConstructorArgument() {
         return this.constructorArgument;
     }
-
     public String getID() {
         return this.id;
     }
-
+    public void setId(String id){
+        this.id = id;
+    }
     public boolean hasConstructorArgumentValues() {
         return !this.constructorArgument.isEmpty();
     }
